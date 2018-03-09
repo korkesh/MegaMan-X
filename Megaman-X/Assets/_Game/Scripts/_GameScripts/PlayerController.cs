4ad4d9a9c2dd6efc309832253827652d50d04202
JGDominik <37196495+JGDominik@users.noreply.github.com>
2018-03-08 21:13:47 -0500

    Oh
    It seemed I had to add a description
    
    anyway,
    
    Added FSM(?)
    Animations for Idle and Running

---
+++ Megaman-X/Assets/_Game/Scripts/_GameScripts/PlayerController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float moveSpeed;
+
+    public bool canMove;
+
+    private Rigidbody2D rb;
+    public Animator an;
+	// Use this for initialization
+	void Start () {
+        an = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //if player cannot move
+        if (!canMove)
+        {
+            return;
+        }
+
+        //if right
+        if (Input.GetAxisRaw("Horizontal") > 0f)
+        {
+            rb.velocity = new Vector2(moveSpeed, rb.velocity.y);
+            transform.localScale = new Vector3(1f, 1f, 1f);
+
+            //set PlayerFSM as Running
+
+        }
+        //if left
+        else if (Input.GetAxisRaw("Horizontal") < 0f)
+        {
+            rb.velocity = new Vector2(-moveSpeed, rb.velocity.y);
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+
+            //set PlayerFSM as Running
+
+        }
+        //if idle
+        else
+        {
+            //set PlayerFSM as Idle
+
+            rb.velocity = new Vector2(0, rb.velocity.y);
+        }
+
+
+        an.SetFloat("speed", Mathf.Abs(rb.velocity.x));
+        an.SetBool("grounded", true);
+
+    }
+}
---
+++ Megaman-X/Assets/_Game/Scripts/_GameScripts/PlayerManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour {
+
+    public enum playerFSM
+    {
+        Idle,
+        Running,
+        Jumping,
+        Falling,
+        Climbing,
+        Dashing,
+        Sliding
+    }
+
+   
+    
+}
